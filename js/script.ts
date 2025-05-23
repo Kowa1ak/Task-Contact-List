06f151ca7312a6495db64e85334d227a29be8c0d
Kowa1ak <danila.tsatsuk22@gmail.com>
2025-05-23 16:49:52 +0300

    feat(sidebar): implement groups sidebar and add deletion confirmation modal window

--- js/script.ts
+++ js/script.ts
@@ -23,3 +23,59 @@ function moveAddButton(): void {
 
 window.addEventListener("DOMContentLoaded", moveAddButton);
 window.addEventListener("resize", moveAddButton);
+
+const groupsBtn = document.querySelector<HTMLButtonElement>(
+  ".header__action-btn--groups"
+);
+const sidebar = document.querySelector<HTMLElement>(".sidebar");
+const overlay = document.querySelector<HTMLElement>(".sidebar-overlay");
+const closeBtn = document.querySelector<HTMLButtonElement>(".sidebar__close");
+
+groupsBtn?.addEventListener("click", () => {
+  sidebar?.classList.add("open");
+  overlay?.classList.add("open");
+});
+
+closeBtn?.addEventListener("click", () => {
+  sidebar?.classList.remove("open");
+  overlay?.classList.remove("open");
+});
+
+overlay?.addEventListener("click", () => {
+  sidebar?.classList.remove("open");
+  overlay?.classList.remove("open");
+});
+
+const modal = document.querySelector<HTMLElement>(".modal");
+const modalOverlay = document.querySelector<HTMLElement>(".modal-overlay");
+const modalClose = document.querySelector<HTMLButtonElement>(".modal__close");
+const modalCancel = document.querySelector<HTMLButtonElement>(
+  ".modal__btn--cancel"
+);
+const modalConfirm = document.querySelector<HTMLButtonElement>(
+  ".modal__btn--confirm"
+);
+const groupRemoveBtns = document.querySelectorAll<HTMLButtonElement>(
+  ".sidebar__item-remove"
+);
+
+groupRemoveBtns.forEach((btn) => {
+  btn.addEventListener("click", () => {
+    sidebar?.classList.remove("open");
+    overlay?.classList.remove("open");
+    modalOverlay?.classList.add("open");
+    modal?.classList.add("open");
+  });
+});
+
+const closeModal = () => {
+  overlay?.classList.remove("open");
+  modalOverlay?.classList.remove("open");
+  modal?.classList.remove("open");
+};
+
+modalClose?.addEventListener("click", closeModal);
+modalCancel?.addEventListener("click", closeModal);
+modalOverlay?.addEventListener("click", closeModal);
+
+modalConfirm?.addEventListener("click", closeModal);
